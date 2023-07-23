646b929861df0fd7ddf469e4c6c5c4d03b7a70f8
Shaker Sharabi <sheko100@icloud.com>
2023-07-23 02:36:49 -0700

    Add exit built-in

--- shell.c
+++ shell.c
@@ -1,45 +1,6 @@
 #include "shell.h"
 
 /**
- * execprg - executes a program
- * @argv: a NULL terminated array contains the program name and its arguments
- * @shname: the name of the shell
- * @isinteractv: either 0 or 1 to
- * detrmine which to create a child process or not
- * @isexist: either 0 or 1 to detrmine if the program was found or not
- *
- * Return: 0 on success
- */
-
-int execprg(char **argv, char *shname, int isinteractv, int isexist)
-{
-	pid_t childpid = 0;
-	int status;
-
-	if (isinteractv && isexist)
-	{
-		childpid = fork();
-		if (childpid == -1)
-		{
-			perror("Creating child process");
-			exit(EXIT_FAILURE);
-		}
-	}
-
-	if (childpid == 0)
-		execve(argv[0], argv, environ);
-	else
-	{
-		if (wait(&status) != -1)
-			return (0);
-		perror("wait");
-	}
-
-	perror(shname);
-	return (-1);
-}
-
-/**
  * shell - starts the shell either in interactive or not-interactive
  * @shname: the path of the shell when executing
  * @isinteractv: either 1 or 0 to
@@ -92,3 +53,64 @@ int shell(char *shname, int isinteractv)
 	return (0);
 }
 
+/**
+ * execprg - executes a program
+ * @argv: a NULL terminated array contains the program name and its arguments
+ * @shname: the name of the shell
+ * @isinteractv: either 0 or 1 to
+ * detrmine which to create a child process or not
+ * @isexist: either 0 or 1 to detrmine if the program was found or not
+ *
+ * Return: 0 on success
+ */
+
+int execprg(char **argv, char *shname, int isinteractv, int isexist)
+{
+	pid_t childpid = 0;
+	int status;
+
+	if (isinteractv && isexist)
+	{
+		childpid = fork();
+		if (childpid == -1)
+		{
+			perror("Creating child process");
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	if (childpid == 0)
+		execve(argv[0], argv, environ);
+	else
+	{
+		if (wait(&status) != -1)
+			return (0);
+		perror("wait");
+	}
+
+	perror(shname);
+	return (-1);
+}
+
+/**
+ * isbuiltin - checks if the command is a built-in and execute the associated
+ * function
+ * @argv: array of arguments strings
+ *
+ * Return: 1 if the command is a built-in
+ * 0 otherwise
+ */
+
+int isbuiltin(char **argv)
+{
+	int status = 0;
+	char *cmd = argv[0];
+
+	if (cmpstr(cmd, "exit"))
+	{
+		free(argv);
+		exit(EXIT_SUCCESS);
+	}
+
+	return (status);
+}
