2fe32eb1e35cce391bebc4e0f1d54d3c6f7e6d3e
Shaker Sharabi <sheko100@icloud.com>
2023-07-23 02:32:46 -0700

    Add cmpstr function

--- str_utils.c
+++ str_utils.c
@@ -30,3 +30,25 @@ char *cpystr(char *s)
 	return (newstr);
 }
 
+/**
+ * cmpstr - compares two strings if they are identical or not
+ * @s1: first str
+ * @s2: second str
+ *
+ * Return: 1 if the strings are identical
+ * 0 otherwise
+ */
+
+int cmpstr(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (s1[i] || s2[i])
+	{
+		if (s1[i] != s2[i])
+			return (0);
+		i++;
+	}
+
+	return (1);
+}
